c2a146d09ffbbe7311dc0485ec6d05411282fe34
maria <maria.topgirl82@gmail.com>
2022-12-17 14:50:20 +0100

    paradigmas de programacion (clases universidad)

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-/**Imperativos VS Declarativos */
+/** Imperativos VS Declarativos */
 
 const listaProgramadores = ["Gorka", "Martin", "Alex", "Leire"];
 
@@ -17,4 +17,32 @@ nombres = [...listaProgramadores];
 
 
 console.log(nombres)
+
 /** Funcionales VS Procedimentales **/
+
+// Suma Procedimental
+
+let suma = 0;
+
+for (let i = 1; 1 <= 10; i++) {
+    suma = suma + i;
+}
+
+// Suma Funcional
+
+// 1 - Definimos la función
+
+function sumar_los_diez_primeros_enteros() {
+    let suma = 0;
+
+    for (let i = 1; 1 <= 10; i++) {
+        suma = suma + i;
+    }
+
+    return suma;
+}
+
+// 2 - Utilizamos la función
+let sumar = sumar_los_diez_primeros_enteros();
+
+console.log(sumar)
